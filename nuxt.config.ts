a5fc448f18ae968669daec5511cca768f0911de2
Yonathan Kevin <yonathan.kevin20@gmail.com>
2024-07-22 21:42:15 +0700

    chore: use bun

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,17 @@ export default defineNuxtConfig({
   build: {
     transpile: ['@vuepic/vue-datepicker']
   },
+
   colorMode: {
     preference: 'light'
   },
+
   dayjs: {
     plugins: ['utc', 'timezone'],
   },
+
   devtools: { enabled: true },
+
   nitro: {
     imports: {
       dirs: [
@@ -17,19 +21,25 @@ export default defineNuxtConfig({
       ]
     }
   },
+
   modules: [
     'dayjs-nuxt',
     '@nuxtjs/supabase',
     '@nuxt/ui',
   ],
+
   runtimeConfig: {
     databaseUrl: '',
   },
+
   supabase: {
     redirect: false,
   },
+
   typescript: {
     strict: true,
     typeCheck: true
-  }
+  },
+
+  compatibilityDate: '2024-07-22'
 })
